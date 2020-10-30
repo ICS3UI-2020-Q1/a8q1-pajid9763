d191c2e2dd9724acc0b59ff442bb63df2b798098
pajid9763 <pajid9763@wrdsb.ca>
2020-10-30 16:51:59 +0000

    finished say hello GUI

--- Main.java
+++ Main.java
@@ -5,7 +5,13 @@ import java.awt.event.*;
 public class Main implements Runnable, ActionListener{
 
   // Class Variables  
-  
+  JPanel mainPanel;
+
+  JTextField nameInput;
+
+  JLabel nameOutput;
+
+  JButton sayHelloButton;
 
 
   // Method to assemble our GUI
@@ -18,8 +24,50 @@ public class Main implements Runnable, ActionListener{
     frame.setSize(800,600);
     // shows the window
     frame.setVisible(true);
+
+    // initialize the main JPanel
+    mainPanel = new JPanel();
+    // turn on the manual layouts
+    mainPanel.setLayout(null);
+    // add the panel to JFrame to see it
+    frame.add(mainPanel);
  
     
+    // initialize the JTextField
+    nameInput = new JTextField();
+
+    // set the location and size for the input field 
+    nameInput.setBounds(200, 100, 400, 30);
+
+    // add the input to the main panel 
+    mainPanel.add(nameInput);
+
+
+    // initialize the JButton
+    sayHelloButton = new JButton("Say Hello");
+
+    // add action listener to the buttons 
+    sayHelloButton.addActionListener(this);
+
+    // give thte button an action command 
+    sayHelloButton.setActionCommand("say hello");
+
+    // set the location and size of the button
+    sayHelloButton.setBounds(340, 400, 120, 20);
+
+    // add the button to the main panel 
+    mainPanel.add(sayHelloButton);
+
+
+    // the the label where the output text will appear
+    nameOutput = new JLabel();
+
+    // set the location and size of the label 
+    nameOutput.setBounds(205, 200, 400, 30);
+
+    // add the label to the main panel 
+    mainPanel.add(nameOutput);
+
 
   }
 
@@ -27,6 +75,14 @@ public class Main implements Runnable, ActionListener{
   public void actionPerformed(ActionEvent e){
     // get the command from the action
     String command = e.getActionCommand();
+
+    if ( command.equals("say hello")){
+      // say hello button was pressed
+      // get the input as a String
+      String outputText = nameInput.getText();
+
+      nameOutput.setText("Hello " + outputText);
+    }
 
   }
 
